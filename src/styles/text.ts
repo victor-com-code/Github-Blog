51223375f3be8f0d7c09ffc7f6a1eb3e567a3baa
Victor-Eduardo <victorelalves@gmail.com>
2023-07-16 13:43:31 -0300

    Estrutura visual - Títulos e Textos

---
+++ src/styles/text.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const TextM = styled.p`
+  font-size: 1rem;
+`
+
+export const TextS = styled.p`
+  font-size: 0.875rem;
+`
---
+++ src/styles/titles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const TitleL = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 700;
+`
+
+export const TitleM = styled.h2`
+  font-size: 1.25rem;
+  font-weight: 700;
+`
+
+export const TitleS = styled.h3`
+  font-size: 1.125rem;
+  font-weight: 700;
+`
